2efe5bf17549ebc99928f2f9b363027146bb8a6d
robowood <satyamb971@gmail.com>
2023-02-03 01:14:58 +0530

    fetch data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
 import { useSelector,useDispatch } from 'react-redux';
 import { useEffect ,useState} from 'react';
+import { sendCartData,fetchCartData } from './store/cart-actions';
+
 import { uiActions } from './store/ui-slice';
 let isInitial=true;
 function App() {
@@ -13,38 +15,16 @@ function App() {
   const notification =useSelector(state=>state.ui.notification)
 
   useEffect(()=>{
-    const sendCartData=async()=>{
-      dispatch(uiActions.showNotification({
-        status:'pending',
-        title:'sending...',
-        message:'sending cart data',
-      }));
-    const response=await fetch('https://advanceredux-e01ef-default-rtdb.firebaseio.com/cart.json',{
-      method:'PUT',
-      body:JSON.stringify(cart),
-
-      });
-      if(!response.ok){
-        throw new Error('sending cart data failed');
-      }
-      dispatch(uiActions.showNotification({
-        status:'success',
-        title:'success...',
-        message:'sending cart data successfully',
-      }));
-    };
+    dispatch(fetchCartData());
+  },[dispatch]);
+  useEffect(()=>{
+    
     if (isInitial) {
       isInitial=false
       return;
     }
-    sendCartData().catch((error)=>{
-      dispatch(uiActions.showNotification({
-        status:'error',
-        title:'Error',
-        message:'sending cart data failed',
-      }));
-    });
-  },[cart]);
+    dispatch(sendCartData(cart));
+  },[cart,dispatch]);
   return (
     <div>
     {notification && <Notification 
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-
+import { uiActions } from './ui-slice';
 const cartSlice = createSlice({
   name: 'cart',
   initialState: {
