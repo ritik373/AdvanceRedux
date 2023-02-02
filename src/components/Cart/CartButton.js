1f168ee715c59406b36a64ad2a975045833cb437
robowood <satyamb971@gmail.com>
2023-02-02 18:16:37 +0530

    AddCartFix

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,15 +1,37 @@
-import classes from './CartButton.module.css';
+// import classes from './CartButton.module.css';
+// import { uiActions } from '../../store/ui-slice';
+// import { useDispatch } from 'react-redux';
+// const CartButton = (props) => {
+//   const dispatch=useDispatch()
+//  const  toggleCartHandler=()=>{
+//   dispatch(uiActions.toggle());
+//  };
+//   return (
+//     <button className={classes.button} onClick={toggleCartHandler}>
+//       <span>My Cart</span>
+//       <span className={classes.badge}>1</span>
+//     </button>
+//   );
+// };
+
+// export default CartButton;
+import { useDispatch, useSelector } from 'react-redux';
+
 import { uiActions } from '../../store/ui-slice';
-import { useDispatch } from 'react-redux';
+import classes from './CartButton.module.css';
+
 const CartButton = (props) => {
-  const dispatch=useDispatch()
- const  toggleCartHandler=()=>{
-  dispatch(uiActions.toggle());
- };
+  const dispatch = useDispatch();
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
