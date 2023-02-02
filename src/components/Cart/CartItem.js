24c860d3b565f3b40c0fcc41853a981ca6b713f7
robowood <satyamb971@gmail.com>
2023-02-02 18:19:34 +0530

    cart fix

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,54 @@
+// import classes from './CartItem.module.css';
+
+// const CartItem = (props) => {
+//   const { title, quantity, total, price } = props.item;
+
+//   return (
+//     <li className={classes.item}>
+//       <header>
+//         <h3>{title}</h3>
+//         <div className={classes.price}>
+//           ${total.toFixed(2)}{' '}
+//           <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
+//         </div>
+//       </header>
+//       <div className={classes.details}>
+//         <div className={classes.quantity}>
+//           x <span>{quantity}</span>
+//         </div>
+//         <div className={classes.actions}>
+//           <button>-</button>
+//           <button>+</button>
+//         </div>
+//       </div>
+//     </li>
+//   );
+// };
+
+// export default CartItem;
+import { useDispatch } from 'react-redux';
+
 import classes from './CartItem.module.css';
+import { cartActions } from '../../store/cart-slice';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const dispatch = useDispatch();
+
+  const { title, quantity, total, price, id } = props.item;
+
+  const removeItemHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
+
+  const addItemHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
 
   return (
     <li className={classes.item}>
@@ -17,8 +64,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
         </div>
       </div>
     </li>
